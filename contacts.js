ca718bf842759d7c6211b89faf2129b08fe0dfce
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-10-03 02:53:13 +0300

    p 2.2. done

---
+++ contacts.js
@@ -0,0 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+const contactsPath = path.parse("C:Projects\01-node-basicsdbcontacts.json");
+
+/*
+ * Раскомментируй и запиши значение
+ * const contactsPath = ;
+ */
+
+// TODO: задокументировать каждую функцию
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
