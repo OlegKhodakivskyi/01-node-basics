d9fe9ca59d8accff024f5b2ee398b8eb23166e43
OlegKhodakivskyi <oleghodakovsky@gmail.com>
2020-11-06 04:10:05 +0200

    can't run server

---
+++ src/server/server.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const path = require("path");
+const cors = require("cors");
+const morgan = require("morgan");
+const contactsRouter = require("../contacts/contacts.routers");
+const AppError = require("../helpers/errApp");
+const errorController = require("../helpers/errController");
+require("dotenv").config();
+
+class CrudServer {
+  start() {
+    this.initServer();
+    this.initMiddlewares();
+    this.initRouters();
+    this.initErrorHandling();
+    this.startListening();
+  }
+
+  initServer() {
+    this.app = express();
+  }
+  initMiddlewares() {
+    this.app.use(express.json());
+    this.app.use(cors({ origin: "http://localhost:3000" }));
+    this.app.use(morgan("combined"));
+  }
+
+  initRouters() {
+    this.app.use("/contacts", contactsRouter);
+  }
+
+  initErrorHandling() {
+    this.app.all("*", (req, res, next) => {
+      next(new AppError(`Can't fint ${req.originalUrl}`, 404));
+    });
+    this.app.use(errorController);
+  }
+
+  startListening() {
+    this.app.listen(process.env.PORT, () => {
+      console.log("Server started listening on port", process.env.PORT);
+    });
+  }
+}
+
+exports.crudServer = new CrudServer();
